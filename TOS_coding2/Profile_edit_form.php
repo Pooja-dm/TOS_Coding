cc937b18e516e2f4eaca98d4012bb06a36c18918
Pooja-dm <106585115+Pooja-dm@users.noreply.github.com>
2022-12-21 18:54:27 +0530

    TOS_Coding2

---
+++ TOS_coding2/Profile_edit_form.php
@@ -0,0 +1,10 @@
+<form action="Profile_update.php" method="post">
+   fname: <input type="text" name="fname"><br>
+ 
+   lname: <input type="text" name="lname"><br>
+ 
+   writer: <input type="text" name="writer"><br>
+   
+   <input type="submit" name="edit">
+   
+</form>
